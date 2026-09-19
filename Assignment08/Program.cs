e55af9faa741ef1f11b431212e8fc8df08cd7a01
Shahdamr2 <170415831+Shahdamr2@users.noreply.github.com>
2026-09-20 01:16:43 +0200

    Question 2

--- Assignment08/Program.cs
+++ Assignment08/Program.cs
@@ -13,6 +13,17 @@
             // Dynamic Binding is decided at runtime depending on the actual object
 
             #endregion
+            #region Question 2
+
+            // a) A sealed class cannot be inherited by another class
+
+            // b) A sealed class prevents inheritance.
+            // A sealed method prevents further overriding of a method
+
+            // c) No, a sealed method cannot be overridden because
+            // it stops further overriding in derived classes
+
+            #endregion
         }
     }
 }
