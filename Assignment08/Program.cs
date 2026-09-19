1c3c5ea6285ed255afd3872babadb1a1a53e89c4
Shahdamr2 <170415831+Shahdamr2@users.noreply.github.com>
2026-09-20 01:07:40 +0200

    Question 1

--- Assignment08/Program.cs
+++ Assignment08/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Question 1
+
+            // a) Overloading means using the same method name with different parameters
+            // Overriding means changing the implementation of a method in the child class
+
+            // b) Static Binding is decided at compile time
+            // Dynamic Binding is decided at runtime depending on the actual object
+
+            #endregion
         }
     }
 }
