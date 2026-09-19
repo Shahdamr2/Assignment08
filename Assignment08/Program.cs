1108e8b5b15998c32b11ff5b0c38e126022f40de
Shahdamr2 <170415831+Shahdamr2@users.noreply.github.com>
2026-09-20 01:28:58 +0200

    Question 7

--- Assignment08/Program.cs
+++ Assignment08/Program.cs
@@ -140,6 +140,52 @@ namespace Assignment08
             #endregion
             #region Question 6
 
+            //DeliveryAddress address =
+            //    new DeliveryAddress("Cairo", "Tahrir Street", 15);
+
+            //StandardShipment standardShipment =
+            //    new StandardShipment(
+            //        "SH101",
+            //        "Laptop",
+            //        3,
+            //        80,
+            //        address);
+
+            //ExpressShipment expressShipment =
+            //    new ExpressShipment(
+            //        "SH102",
+            //        "Mobile",
+            //        2,
+            //        100,
+            //        address,
+            //        50);
+
+            //InternationalShipment internationalShipment =
+            //    new InternationalShipment(
+            //        "SH103",
+            //        "Tablet",
+            //        4,
+            //        120,
+            //        address,
+            //        "USA",
+            //        70);
+
+            //standardShipment.PrintShipment();
+
+            //Console.WriteLine();
+
+            //expressShipment.PrintShipment();
+
+            //Console.WriteLine();
+
+            //internationalShipment.PrintShipment();
+
+            #endregion
+            #region Question 7
+
+            DeliveryCenter center =
+                new DeliveryCenter("Main Delivery Center");
+
             DeliveryAddress address =
                 new DeliveryAddress("Cairo", "Tahrir Street", 15);
 
@@ -170,15 +216,11 @@ namespace Assignment08
                     "USA",
                     70);
 
-            standardShipment.PrintShipment();
+            center.AddShipment(standardShipment);
+            center.AddShipment(expressShipment);
+            center.AddShipment(internationalShipment);
 
-            Console.WriteLine();
-
-            expressShipment.PrintShipment();
-
-            Console.WriteLine();
-
-            internationalShipment.PrintShipment();
+            center.PrintAllShipments();
 
             #endregion
         }
