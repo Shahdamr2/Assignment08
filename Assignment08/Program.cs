dacd5964bb415b234634f248e1c867a1914919fc
Shahdamr2 <170415831+Shahdamr2@users.noreply.github.com>
2026-09-21 01:27:32 +0200

    Question 1

--- Assignment08/Program.cs
+++ Assignment08/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Question 1
+
+            // Abstraction means hiding implementation details
+            // and showing only the important features of an object
+
+            // It helps reduce complexity and focuses on what the object does
+            // instead of how it does it
+
+            #endregion
+
         }
     }
 }
