74385b1e349988e6b904bb872c9826178460d28f
Shahdamr2 <170415831+Shahdamr2@users.noreply.github.com>
2026-09-21 01:47:45 +0200

    Practical Question 6

---
+++ Assignment08/Reports/DeliveryReport.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Assignment08.Interfaces;
+
+namespace Assignment08.Reports
+{
+    public class DeliveryReport
+    {
+        public void PrintShipment(ITrackable shipment)
+        {
+            Console.WriteLine(shipment.GetTrackingStatus());
+        }
+
+        public void PrintInsurance(IInsurable shipment)
+        {
+            Console.WriteLine($"Insurance Cost: {shipment.CalculateInsurance()} EGP");
+        }
+    }
+}
