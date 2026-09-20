0695f4d0eedafc0c4a702744aa22a040c80b8b3e
Shahdamr2 <170415831+Shahdamr2@users.noreply.github.com>
2026-09-21 01:29:29 +0200

    Question 2

--- Assignment08/Program.cs
+++ Assignment08/Program.cs
@@ -13,6 +13,19 @@
             // instead of how it does it
 
             #endregion
+            #region Question 2
+
+            // a) An abstract class can contain fields, properties, constructors,
+            // normal methods, and abstract methods
+            // An interface is mainly used to define a contract
+
+            // b) We use an interface when different classes need
+            // to follow the same contract
+
+            // c) A class can inherit from only one abstract class,
+            // but it can implement multiple interfaces
+
+            #endregion
 
         }
     }
